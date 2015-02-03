4665847a05f07080435bb96df3eb9401b37ffc64
gekka <git-gekka@hotmail.com>
2015-02-03 20:57:08 +0900

    20150203

---
+++ TextBoxOnImageReport/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace ReportsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合は true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer によって生成されたコード
+
+        /// <summary>
+        /// Designer のサポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 356);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ TextBoxOnImageReport/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReportsApplication2
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        public Form1()
+        {
+            InitializeComponent();
+
+            reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            reportViewer1.Dock = DockStyle.Fill;          
+            reportViewer1.LocalReport.ReportPath = "Report1.rdlc";
+            this.reportViewer1.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
+            this.Controls.Add(reportViewer1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
